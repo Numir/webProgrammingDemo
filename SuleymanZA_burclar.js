b9b8bde1bd82bccc819f63f74c62044d2d724fbf
Numir <numirselman@gmail.com>
2020-12-01 18:19:32 +0100

    Update SuleymanZA_burclar.js

--- SuleymanZA_burclar.js
+++ SuleymanZA_burclar.js
@@ -1,5 +1,4 @@
-console.log("Süleyman ZA");
-
+console.log('Süleyman ZA');
 let month = 3; // mart ayi
 let day = 12; //ayin 12. günp
 
@@ -11,39 +10,33 @@ Bazı açıkları var. şimdilik idare eder :)
  */
 let gun = day; // 1--31
 let ay = month; // 1--12
-let burc = "Hatadır olur gider..";
+let burc = 'Hatadır olur gider..';
 if ((ay == 1 && gun >= 22) || (ay == 2 && gun <= 19)) {
-    console.log("Burç = Kova");
+     console.log('Burç = Kova');
 } else if ((ay == 2 && gun >= 20) || (ay == 3 && gun <= 20)) {
-    console.log("Burç = Balık");
+     console.log('Burç = Balık');
 } else if ((ay == 3 && gun >= 21) || (ay == 4 && gun <= 20)) {
-    console.log("Burç = Koç");
+     console.log('Burç = Koç');
 } else if ((ay == 4 && gun >= 21) || (ay == 5 && gun <= 21)) {
-    console.log("Burç = Boğa");
+     console.log('Burç = Boğa');
 } else if ((ay == 5 && gun >= 22) || (ay == 6 && gun <= 22)) {
-    console.log("Burç = İkizler");
+     console.log('Burç = İkizler');
 } else if ((ay == 6 && gun >= 23) || (ay == 7 && gun <= 22)) {
-    console.log("Burç = Yengeç");
+     console.log('Burç = Yengeç');
 } else if ((ay == 7 && gun >= 23) || (ay == 8 && gun <= 22)) {
-    console.log("Burç = Aslan");
+     console.log('Burç = Aslan');
 } else if ((ay == 8 && gun >= 23) || (ay == 9 && gun <= 22)) {
-    console.log("Burç = Başak");
+     console.log('Burç = Başak');
 } else if ((ay == 9 && gun >= 23) || (ay == 10 && gun <= 22)) {
-    console.log("Burç = Terazi");
+     console.log('Burç = Terazi');
 } else if ((ay == 10 && gun >= 23) || (ay == 11 && gun <= 21)) {
-    console.log("Burç = Akrep");
+     console.log('Burç = Akrep');
 } else if ((ay == 11 && gun >= 22) || (ay == 12 && gun <= 21)) {
-    console.log("Burç = Yay");
+     console.log('Burç = Yay');
 } else if ((ay == 12 && gun >= 22) || (ay == 1 && gun <= 21)) {
-    console.log("Burç = Oğlak");
+     console.log('Burç = Oğlak');
 } else {
-    console.log(
-        "Birşeyler yanlış gitti.. Bilgileri kontrol ediniz..\n",
-        "Girdiğiniz Gün =",
-        gun,
-        "Girdiğiniz Ay =",
-        ay
-    );
+     console.log('Birşeyler yanlış gitti.. Bilgileri kontrol ediniz..\n', 'Girdiğiniz Gün =', gun, 'Girdiğiniz Ay =', ay);
 }
 
 /* 
